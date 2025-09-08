c3770d5b806d70e4db55c61fc738bc75cbf6413b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-09-08 17:49:53 +0200

    [~] Task: Climbing Stairs

---
+++ Tasks/ClimbingStairsTask.py
@@ -0,0 +1,17 @@
+def climbStairs(self, n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    prev1, prev2 = 2, 1
+
+    for i in range(3, n + 1):
+        current = prev1 + prev2
+        prev2 = prev1
+        prev1 = current
+
+    return prev1
+
+#https://leetcode.com/problems/climbing-stairs/description/
+
