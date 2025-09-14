6bb600e9a097f252a825288d82d5291e5f411ef5
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-09-14 21:20:37 +0200

    [~] Task: Counting Bits

---
+++ Tasks/CountingBitsTask.py
@@ -0,0 +1,15 @@
+def countBits(self, n):
+    """
+    :type n: int
+    :rtype: List[int]
+    """
+    result = [0, ]
+    iterator = 1
+
+    while n >= iterator:
+        to_binary = bin(iterator)
+        result.append(to_binary.count("1"))
+        iterator += 1
+
+    return result
+#https://leetcode.com/problems/counting-bits/description/
