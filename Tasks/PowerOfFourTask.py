cdac0ef0693ef223a098e744b1076735e2d005a2
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-09-14 21:48:05 +0200

    [~] Task: Power of Four

---
+++ Tasks/PowerOfFourTask.py
@@ -0,0 +1,17 @@
+import math
+
+def isPowerOfFour(self, n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+
+    if n <= 0:
+        return False
+
+    while n % 4 == 0:
+        n //= 4
+
+    return n == 1
+
+    #https://leetcode.com/problems/power-of-four/description/
