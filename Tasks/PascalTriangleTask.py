d7a279f37370e8469d5a2ff83570c198f7351ddf
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-09-23 12:44:02 +0200

    [~] Task: Pascal's Triangle

---
+++ Tasks/PascalTriangleTask.py
@@ -0,0 +1,19 @@
+def generate(self, numRows):
+    """
+    :type numRows: int
+    :rtype: List[List[int]]
+    """
+
+    triangle = []
+
+    for i in range(numRows):
+        row = [1] * (i + 1)
+
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    return triangle
+
+#https://leetcode.com/problems/pascals-triangle/description/
