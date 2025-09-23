ab71e755a1a393fe0d7dcf3e3394a46fa9feb3b4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-09-23 12:49:24 +0200

    [~] Task: Pascal's Triangle II

---
+++ Tasks/PascalTriangleIITask.py
@@ -0,0 +1,19 @@
+def getRow(self, rowIndex):
+    """
+    :type rowIndex: int
+    :rtype: List[int]
+    """
+    row = [1]
+
+    for i in range(1, rowIndex + 1):
+        new_row = [1]
+
+        for j in range(1, i):
+            new_row.append(row[j - 1] + row[j])
+
+        new_row.append(1)
+        row = new_row
+
+    return row
+
+#https://leetcode.com/problems/pascals-triangle-ii/description/
