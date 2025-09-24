afdf01a4271af48c55f067041d66d947a5fe9852
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-09-24 19:05:27 +0200

    [~] Task: Best Time to Buy and Sell Stock

---
+++ Tasks/BestTimeToBuyAndSellStockTask.py
@@ -0,0 +1,20 @@
+def maxProfit(self, prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    if not prices:
+        return 0
+
+    min_price = float('inf')
+    result = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > result:
+            result = price - min_price
+
+    return result
+
+#https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
