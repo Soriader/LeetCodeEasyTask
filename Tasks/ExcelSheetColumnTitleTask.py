df710049a2f411cd577551d5ea63c3f716d62f84
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-09-26 16:34:41 +0200

    [~] Task: Excel Sheet Column Title

---
+++ Tasks/ExcelSheetColumnTitleTask.py
@@ -0,0 +1,16 @@
+import string
+
+def convertToTitle(self, columnNumber):
+    num_to_letter = {i + 1: letter for i, letter in enumerate(string.ascii_uppercase)}
+
+    result = ""
+
+    while columnNumber > 0:
+        remainder = columnNumber % 26
+        if remainder == 0:
+            remainder = 26
+        result = num_to_letter[remainder] + result
+        columnNumber = (columnNumber - remainder) // 26
+
+    return result
+#https://leetcode.com/problems/excel-sheet-column-title/description/
