5f11b8b62a0fe14f57cd5581aa5f2772fce9c71e
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-10-07 17:59:35 +0200

    [~] Task: Maximum Product Difference Between Two Pairs

---
+++ Tasks/MaximumProductDifferenceBetweenTwoPairsTask.py
@@ -0,0 +1,9 @@
+def maxProductDifference(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    nums.sort()
+    return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
+
+#https://leetcode.com/problems/maximum-product-difference-between-two-pairs/description/
