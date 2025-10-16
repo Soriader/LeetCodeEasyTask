0bec760e1f1352f4bd057989f8ad151b07fe0734
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-10-16 15:37:36 +0200

    [~] Task: Replace All ?'s to Avoid Consecutive Repeating Characters

---
+++ Tasks/AvoidConsecutiveRepeatingCharactersTask.py
@@ -0,0 +1,22 @@
+def modifyString(self, s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    s = list(s)
+    n = len(s)
+
+    for i in range(n):
+        if s[i] == '?':
+            left = s[i - 1] if i > 0 else ''
+            right = s[i + 1] if i < n - 1 else ''
+
+            for char in 'abcdefghijklmnopqrstuvwxyz':
+                if char != left and char != right:
+                    s[i] = char
+                    break
+
+    return ''.join(s)
+
+
+#https://leetcode.com/problems/replace-all-s-to-avoid-consecutive-repeating-characters/description/
