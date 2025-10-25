61738edf60526d2b9668a0d79eade5ad20880e9a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-10-25 09:29:52 +0200

    [~] Task: Calculate Money in Leetcode Bank

---
+++ Tasks/CalculateMoneyInLeetcodeBankTask.py
@@ -0,0 +1,23 @@
+def totalMoney(self, n):
+    """
+    :type n: int
+    :rtype: int
+    """
+
+    monday = 1
+    iterator = 0
+    result = 0
+    money = monday
+
+    while iterator < n:
+        result += money
+        money += 1
+        iterator += 1
+
+        if iterator % 7 == 0:
+            monday += 1
+            money = monday
+
+    return result
+
+#https://leetcode.com/problems/calculate-money-in-leetcode-bank/description/?envType=daily-question&envId=2025-10-25
