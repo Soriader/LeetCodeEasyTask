ca447d8f36b64032ff570d4d1a4855ee58d433dc
NewLavoisier <kacperjwawrzyniak@gmail.com>
2026-03-06 17:02:50 +0100

    [~] Task: Check if Binary String Has at Most One Segment of Ones

---
+++ Tasks/CheckIfBinaryStringHasAtMostOneSegmentOfOnesTask.py
@@ -0,0 +1,5 @@
+def checkOnesSegment(self, s: str) -> bool:
+
+    return "01" not in s
+
+#https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/?envType=daily-question&envId=2026-03-06
